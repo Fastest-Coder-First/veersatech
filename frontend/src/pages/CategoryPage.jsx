56cb8fbfd95a54cf5e27a9101e580107cbe327c3
Daya Singh <lubana1331@gmail.com>
2023-06-24 17:35:09 +0530

    ADD category

--- frontend/src/pages/CategoryPage.jsx
+++ frontend/src/pages/CategoryPage.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useAuth } from "../contexts/AuthContext";
 import { TableComponent, SearchFilters } from "../Components/Finances";
 import NavbarComponent from "../Components/Common/NavbarComponent";
+import {getCategory}  from "../apis/category.js";
+import { Button } from "react-bootstrap";
+import { useState } from 'react';
 
 const Category = () => {
   const { currentUser, loggedIn, setLoggedIn } = useAuth();
+  const [search, setSearch] = useState('');
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+  },[]);
+
+  // create on search using event value on data
+  const onSearch = (value) => {
+    setSearch(value);
+    const filterData = data.filter((item) => {
+      return item.name.toLowerCase().includes(value.toLowerCase());
+    });
+    setData(filterData);
+  };
 
   return (
     <div className="finances">
@@ -12,8 +29,7 @@ const Category = () => {
       <br />
       <br />
       <div className="container">
-        <SearchFilters />
-        <TableComponent name="Category" btnType="Add Category"/>
+        <TableComponent tableData={data} name="Category" btnType="Add Category" createNewItemType="AddCategory"/>
       </div>
     </div>
   );
