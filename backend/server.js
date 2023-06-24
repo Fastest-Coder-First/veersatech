3a44add8a2fe2b2353ceb377e160b56e5a85937e
Daya Singh <lubana1331@gmail.com>
2023-06-24 18:42:07 +0530

    allow cors

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json())
 app.use(express.static('public')); 
 app.use('/images', express.static('images'));
 var corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: '*',
     optionsSuccessStatus: 200 
 }
 
