dc634293bd9df64dd8fe39c026618bd8d8abc820
Chaitanya Gupta <chaitanya.gupta@veersatech.com>
2023-06-24 11:53:45 +0530

    contexts setup for auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,56 @@
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { DashboardPage, HomePage, NotFoundPage } from "./pages";
+import { useEffect } from "react";
+import Swal from "sweetalert2";
+import { loadUser, setAuthToken } from "./apis";
+
+import { useAuth } from "./contexts/AuthContext";
+import PrivateRoutes from "./routes/PrivateRoutes";
 
 function App() {
+  const navigate = useNavigate();
+  const { setCurrentUser, loggedIn, setLoggedIn } = useAuth();
+  const authToken = localStorage.getItem("token");
+
+  useEffect(() => {
+    if (authToken) {
+      setAuthToken(authToken);
+
+      const getUser = async () => {
+        try {
+          const { user } = await loadUser();
+          setCurrentUser(user);
+          setLoggedIn(true);
+          navigate("/dashboard");
+        } catch (error) {
+          console.log("error: ", error);
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Something went wrong!",
+          });
+          localStorage.removeItem("authToken");
+          setLoggedIn(false);
+          navigate("/");
+        }
+      };
+      getUser();
+    }
+  }, [authToken]);
+
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route
+        element={
+          <PrivateRoutes
+            loggedIn={loggedIn || localStorage.getItem("authToken")}
+          />
+        }
+      >
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Route>
 
-      {/* This is the catch all route, if no other route matches, then this route will be used */}
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,17 +13,20 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AuthProvider } from "./contexts/AuthContext";
 // Create a client
 const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <App />
-      </Router>
-    </QueryClientProvider>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <App />
+        </Router>
+      </QueryClientProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
 
